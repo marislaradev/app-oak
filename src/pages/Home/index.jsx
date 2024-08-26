c0700602ca0bf0b23fbb112ee861c3172f34392b
Mariana da Silva Lara <marislaradev@outlook.com>
2024-08-26 11:00:06 -0300

    refactor(home-page): rename title

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,7 +5,7 @@ function Home() {
     <section className="min-h-screen flex items-center justify-center w-full bg-greenOak dark:bg-gray-950">
       <div className="bg-white dark:bg-gray-900 shadow-md rounded-lg px-8 py-6 max-w-md">
         <h1 className="text-2xl font-bold text-center mb-4 dark:text-gray-200">
-          Oak Store
+          Controle de entregas - 584
         </h1>
         <ProductForm />
       </div>
