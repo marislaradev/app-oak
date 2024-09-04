0513287b2c6b6d84c5fe7a9e2f6bc6f8318b4367
Mariana da Silva Lara <marislaradev@outlook.com>
2024-09-03 21:40:39 -0300

    feat: delivery registration form

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,14 @@
-import ProductForm from "../../components/ProductForm";
+import DeliveryForm from "../../components/DeliveryForm";
 
 function Home() {
   return (
     <section className="min-h-screen flex items-center justify-center w-full bg-greenOak dark:bg-gray-950">
       <div className="bg-white dark:bg-gray-900 shadow-md rounded-lg px-8 py-6 max-w-md">
         <h1 className="text-2xl font-bold text-center mb-4 dark:text-gray-200">
-          Controle de entregas - 584
+          PanVel Foz do Iguaçu - 584
         </h1>
-        <ProductForm />
-      </div>
+        <DeliveryForm />
+      </div> 
     </section>
   );
 }
